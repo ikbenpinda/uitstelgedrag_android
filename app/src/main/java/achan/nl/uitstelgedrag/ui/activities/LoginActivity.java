30d0d94cf6e662116f697e892563527a9de85dc5
Etienne Cooijmans <etiennecooijmans@gmail.com>
2016-06-27 11:30:57 +0200

    Added login screen.

---
+++ app/src/main/java/achan/nl/uitstelgedrag/ui/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package achan.nl.uitstelgedrag.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import achan.nl.uitstelgedrag.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
